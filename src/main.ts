3214ec9ce23d3acda1417193809a87e5ffccb2a6
angelmcabgom <angelmcabgom@gmail.com>
2026-02-21 13:34:51 +0100

    feat: swagger init config and some advance on url encoder

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('vault-link')
+    .setDescription('s3 type vault for homelab')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // 'api' is the path where the docs will be hosted (e.g., localhost:3000/api)
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
--- src/url-encoder/url-encoder.service.ts
+++ src/url-encoder/url-encoder.service.ts
@@ -1,12 +1,13 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { Base62Utils } from 'src/utils/base62.util';
 
 @Injectable()
 export class UrlEncoderService {
   private readonly logger = new Logger(UrlEncoderService.name);
 
-  encoderUrl(url: string) {
+  encoderUrl(url: number) {
     try {
-      const encodedUrl = crypto(,);
+      const encodedUrl = Base62Utils.base62encoder(url);
 
       return encodedUrl;
     } catch (err) {
