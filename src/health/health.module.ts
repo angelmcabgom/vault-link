16173f866888ffba6bc4a87540e5ecfa79d971cc
angel <angelmcabgom@gmail.com>
2026-03-01 13:14:16 +0100

    fix: correct imports

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { TerminusModule } from '@nestjs/terminus';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   controllers: [HealthController],
-  imports: [TerminusModule],
+  imports: [TerminusModule, HttpModule],
 })
 export class HealthModule {}
